86e89fef6f35d650d64f1e5c4e697923ea493dd5
Artem Zakharchenko <kettanaito@gmail.com>
2020-05-29 14:19:35 +0200

    Storybook: Improves the steps explanation in "preview.js"

--- examples/with-storybook/.storybook/preview.js
+++ examples/with-storybook/.storybook/preview.js
@@ -3,6 +3,8 @@ import { useState, useEffect, addDecorator } from '@storybook/client-api'
 import { worker } from '../src/mocks'
 
 // Start the Mock Service Worker outside of a story's scope.
+// There should be a single instance of the Service Worker
+// that affects all the network communication that stories may issue.
 const mockPromise = worker.start()
 
 addDecorator((storyFn) => {
