7b771497e61bdc025d7e264383e9b8657c2fa78c
Artem Zakharchenko <kettanaito@gmail.com>
2020-05-29 14:21:33 +0200

    Storybook: Adds explanatory comment on why the "worker" is exported

--- examples/with-storybook/src/mocks.js
+++ examples/with-storybook/src/mocks.js
@@ -1,5 +1,7 @@
 import { setupWorker, rest } from 'msw'
 
+// Export the worker reference, so we can await the activation on Storybook's runtime.
+// You can use this reference to start the worker for local development as well.
 export const worker = setupWorker(
   rest.get('/user', (req, res, ctx) => {
     return res(ctx.json({ firstName: 'John', lastName: 'Maverick' }))
