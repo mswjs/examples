7bd23a85c71328467e4fcff56e87a98d77015b94
Artem Zakharchenko <kettanaito@gmail.com>
2020-06-30 10:17:59 +0200

    Adds a shell script to use locally packed "msw" package

---
+++ scripts/use-packed-msw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+tarball_path=$1
+
+echo "Using local tarbal $tarball_path"
+
+# Upack the tarball
+tar zxvf $tarball_path -C $PWD
+
+echo "Successfully unpacked $tarball_path"
+package_path="$PWD/package"
+
+echo "Using unpacked package at $package_path"
+ls -la $package_path
+
+# Install unpacked tarball to all sub-repositories
+echo "Installing local package in the workspace..."
+yarn workspaces run add file:"$package_path" --no-lockfile
