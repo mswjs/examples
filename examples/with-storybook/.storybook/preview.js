bdcbe2b88e85b20ebe9aba927d55ce96973343c0
Artem Zakharchenko <kettanaito@gmail.com>
2020-07-01 12:44:56 +0200

    Storybook: Conditionally starts "setupWorker" during tests

--- examples/with-storybook/.storybook/preview.js
+++ examples/with-storybook/.storybook/preview.js
@@ -1,6 +1,11 @@
-import { worker } from '../src/mocks'
+// Storybook executes this module in both bootstap phase (Node)
+// and a story's runtime (browser). However, cannot call `setupWorker`
+// in Node environment, so need to check if we're in a browser.
+if (typeof global.process === 'undefined') {
+  const { worker } = require('../src/mocks')
 
-// Start the mocking when each story is loaded.
-// Repetitive calls to the `.start()` method do not register a new worker,
-// but check whether there's an existing once, reusing it, if so.
-worker.start()
+  // Start the mocking when each story is loaded.
+  // Repetitive calls to the `.start()` method do not register a new worker,
+  // but check whether there's an existing once, reusing it, if so.
+  worker.start()
+}
