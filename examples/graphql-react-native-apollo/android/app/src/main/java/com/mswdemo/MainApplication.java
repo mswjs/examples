02d58dc7c67c9af4d0d041a71d6bf93e7b314520
Artem Zakharchenko <kettanaito@gmail.com>
2020-10-22 11:26:37 +0200

    Renames React Native project for GraphQL example

--- examples/graphql-react-native-apollo/android/app/src/main/java/com/mswdemo/MainApplication.java
+++ examples/graphql-react-native-apollo/android/app/src/main/java/com/mswdemo/MainApplication.java
@@ -1,4 +1,4 @@
-package com.mswdemo;
+package com.GraphQLReactNativeApollo;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.mswdemo.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.GraphQLReactNativeApollo.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
