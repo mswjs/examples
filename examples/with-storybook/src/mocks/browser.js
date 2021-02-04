667e5637643ab39eb06681cda8ee461f27760e0b
Artem Zakharchenko <kettanaito@gmail.com>
2021-02-04 16:00:14 +0100

    Storybook: Removes the console.log statement

--- examples/with-storybook/src/mocks/browser.js
+++ examples/with-storybook/src/mocks/browser.js
@@ -1,8 +1,6 @@
 import { setupWorker } from 'msw'
 import { handlers } from './handlers'
 
-console.log('setupWorker', typeof window, typeof global.process)
-
 // Export the worker instance, so we can await the activation on Storybook's runtime.
 // You can use this reference to start the worker for local development as well.
 export const worker =
