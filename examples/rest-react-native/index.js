f66f4999ba13da7af0405edbb149e162ba68f456
Taylor Kline <tkline@thebhwgroup.com>
2021-07-18 10:12:57 -0500

    Add react-native-url-polyfill
    Without this polyfill, calling `server.start()` will result in an Error: not implemented message followed by Error: Invariant Violation: Module AppRegistry is not a registered callable module (calling runApplication). A frequent cause of the error is that the application entry file path is incorrect.
    
    This is due to the React Native URL polyfill that throws Not Implemented exceptions for functions that MSW calls.

--- examples/rest-react-native/index.js
+++ examples/rest-react-native/index.js
@@ -8,6 +8,7 @@ import {name as appName} from './app.json';
 
 // Start the mocking conditionally
 if (__DEV__) {
+  require('react-native-url-polyfill/auto');
   const {server} = require('./src/api/mocks/server.ts');
   server.listen();
 }
