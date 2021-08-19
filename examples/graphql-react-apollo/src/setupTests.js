a8bb071d997a24f3a7f7107cbb414659c22ccc5a
Charley <ctpugmire@gmail.com>
2021-08-19 09:57:28 -0500

    add cache clearing to apollo example

--- examples/graphql-react-apollo/src/setupTests.js
+++ examples/graphql-react-apollo/src/setupTests.js
@@ -3,11 +3,18 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
+import { client } from './ApolloClient'
 import { server } from './mocks/server'
 
 beforeAll(() => {
   // Enable the mocking in tests.
   server.listen()
+})
+
+beforeEach(() => {
+  // Ensure Apollo cache is cleared between tests.
+  // https://www.apollographql.com/docs/react/api/core/ApolloClient/#ApolloClient.clearStore
+  return client.clearStore()
 })
 
 afterEach(() => {
