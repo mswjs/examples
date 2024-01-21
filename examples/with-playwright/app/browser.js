55d37625540528ef8f0e9a7f99cfa31b1f254d5d
Sam Kvale <sam.kvale@sap.com>
2024-01-21 09:37:00 -0600

    fix: only commit the script type module change

--- examples/with-playwright/app/browser.js
+++ examples/with-playwright/app/browser.js
@@ -2,4 +2,4 @@ import { setupWorker } from 'msw/browser'
 import { handlers } from './handlers'
 
 const worker = setupWorker(...handlers)
-window.workerStartPromise = worker.start()
+worker.start()
