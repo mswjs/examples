06a1218a367d391e21dd63e18f24683d0b7ceed4
Artem Zakharchenko <kettanaito@gmail.com>
2024-04-07 13:26:41 +0200

    add e2e tests

--- examples/with-websocket/src/main.tsx
+++ examples/with-websocket/src/main.tsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+async function enableMocking() {
+  if (import.meta.env.VITE_USE_MOCKS) {
+    const { worker } = await import('../mocks/browser.ts')
+    await worker.start({
+      onUnhandledRequest(request, print) {
+        const url = new URL(request.url)
+
+        // Ignore HTTP requests triggered by Vite's HMR.
+        if (!/\.(ts|tsx|css)/.test(url.pathname)) {
+          print.warning()
+        }
+      },
+    })
+  }
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})
--- examples/with-websocket/src/vite-env.d.ts
+++ examples/with-websocket/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_USE_MOCKS: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
