fc467a309745b7a0564852108717d2a782dc474a
Artem Zakharchenko <kettanaito@gmail.com>
2024-07-26 12:13:51 +0200

    use one msw-provider file

--- examples/with-next/app/msw-provider.tsx
+++ examples/with-next/app/msw-provider.tsx
@@ -1,9 +1,36 @@
-import { MSWClientProvider } from './msw-provider-client'
+'use client'
+
+import { Suspense, use } from 'react'
+
+let mockingPromise: Promise<boolean> | undefined
+
+// if we're running in the browser, start the worker
+if (typeof window !== 'undefined') {
+  const { worker } = require('../mocks/browser')
+  mockingPromise = worker.start()
+}
 
 export function MSWProvider({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  return <MSWClientProvider>{children}</MSWClientProvider>
+  // if MSW is enabled, we need to wait for the worker to start, so we wrap the
+  // children in a Suspense boundary until the worker is ready
+  return (
+    <Suspense fallback={null}>
+      <MSWProviderWrapper>{children}</MSWProviderWrapper>
+    </Suspense>
+  )
+}
+
+function MSWProviderWrapper({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  if (mockingPromise) {
+    use(mockingPromise!)
+  }
+  return children
 }
