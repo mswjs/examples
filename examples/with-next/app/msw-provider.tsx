dc7a437911ca00c07e9a5fc7e7ab543057271372
Artem Zakharchenko <kettanaito@gmail.com>
2024-08-29 15:39:26 +0200

    polish client-side `use()` hook

--- examples/with-next/app/msw-provider.tsx
+++ examples/with-next/app/msw-provider.tsx
@@ -2,21 +2,27 @@
 
 import { Suspense, use } from 'react'
 
-let mockingPromise: Promise<boolean> | undefined
-
-// if we're running in the browser, start the worker
-if (typeof window !== 'undefined') {
-  const { worker } = require('../mocks/browser')
-  mockingPromise = worker.start()
-}
+const mockingEnabledPromise =
+  typeof window !== 'undefined'
+    ? import('../mocks/browser').then(async ({ worker }) => {
+        await worker.start({
+          onUnhandledRequest(request, print) {
+            if (request.url.includes('_next')) {
+              return
+            }
+            print.warning()
+          },
+        })
+      })
+    : Promise.resolve()
 
 export function MSWProvider({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  // if MSW is enabled, we need to wait for the worker to start, so we wrap the
-  // children in a Suspense boundary until the worker is ready
+  // If MSW is enabled, we need to wait for the worker to start,
+  // so we wrap the children in a Suspense boundary until it's ready.
   return (
     <Suspense fallback={null}>
       <MSWProviderWrapper>{children}</MSWProviderWrapper>
@@ -29,8 +35,6 @@ function MSWProviderWrapper({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  if (mockingPromise) {
-    use(mockingPromise!)
-  }
+  use(mockingEnabledPromise)
   return children
 }
